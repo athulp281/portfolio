5b826b6d0bfd18a3551175d9f8b0183c58dccfea
athul <athulpranoor@gmail.com>
2023-10-24 14:08:25 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
@@ -11,46 +13,62 @@ import Loader from "./components/Loader";
 // import { MyContextProvider } from "./useContext/MyContextProvider";
 
 function App() {
-  const [showLoader, setShowLoader] = useState(true);
-  useEffect(() => {
-    // Hide the loader after 1 second
-    const loaderTimeout = setTimeout(() => {
-      setShowLoader(false);
-    }, 1500);
+    const [showLoader, setShowLoader] = useState(true);
+    useEffect(() => {
+        // Hide the loader after 1 second
+        const loaderTimeout = setTimeout(() => {
+            setShowLoader(false);
+        }, 1500);
 
-    return () => {
-      clearTimeout(loaderTimeout); // Clear the timeout to prevent memory leaks
-    };
-  }, []);
-  return (
-    <>
-      {showLoader && <Loader />}
-      {!showLoader && (
-        <MyContextProvider>
-          <Router>
-            <div>
-              <Routes>
-                <Route exact path="/" element={<Home />}></Route>
-                <Route exact path="/contact" element={<Contact />}></Route>
-                <Route exact path="/head" element={<Header />}></Route>
-                <Route
-                  exact
-                  path="/featurebox"
-                  element={<FeatureBox />}
-                ></Route>
-                <Route exact path="/aboutbox" element={<AboutBox />}></Route>
-                <Route
-                  exact
-                  path="/projectbox"
-                  element={<ProjectBox />}
-                ></Route>
-              </Routes>
-            </div>
-          </Router>
-        </MyContextProvider>
-      )}
-    </>
-  );
+        return () => {
+            clearTimeout(loaderTimeout); // Clear the timeout to prevent memory leaks
+        };
+    }, []);
+    return (
+        <>
+            {showLoader && <Loader />}
+            {!showLoader && (
+                <MyContextProvider>
+                    <Router>
+                        <div>
+                            <Routes>
+                                <Route
+                                    exact
+                                    path="/"
+                                    element={<Home />}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/contact"
+                                    element={<Contact />}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/head"
+                                    element={<Header />}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/featurebox"
+                                    element={<FeatureBox />}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/aboutbox"
+                                    element={<AboutBox />}
+                                ></Route>
+                                <Route
+                                    exact
+                                    path="/projectbox"
+                                    element={<ProjectBox />}
+                                ></Route>
+                            </Routes>
+                        </div>
+                    </Router>
+                </MyContextProvider>
+            )}
+        </>
+    );
 }
 
 export default App;
